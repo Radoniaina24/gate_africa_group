e4edde67dae62a3c0d6c769e8b8f076a4ec383a3
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2025-06-30 08:45:52 +0300

    v11.1

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import { NextIntlClientProvider } from "next-intl";
 
 const inter = Montserrat({
   subsets: ["latin"],
@@ -20,7 +21,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={` ${inter.className}`}>{children}</body>
+      <body className={` ${inter.className}`}>
+        {" "}
+        <NextIntlClientProvider>{children}</NextIntlClientProvider>
+      </body>
     </html>
   );
 }
