a1f8d6b3076b316764f2159c1aca52ee4323fc68
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2025-09-05 08:27:37 +0300

    v13.4

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -65,9 +65,9 @@ const Navbar = () => {
                 className="h-12 w-auto rounded-lg"
               />
             </div>
-            <div className="text-center py-4">
+            <div className="hidden md:block text-center py-4 md:text-md lg:text-lg">
               <div
-                className="text-xl font-extrabold bg-gradient-to-r from-red-600 via-red-400 to-red-600 bg-clip-text text-transparent drop-shadow-sm"
+                className=" font-extrabold bg-gradient-to-r from-red-600 via-red-400 to-red-600 bg-clip-text text-transparent drop-shadow-sm"
                 dangerouslySetInnerHTML={{ __html: t.raw("title") }}
               />
             </div>
